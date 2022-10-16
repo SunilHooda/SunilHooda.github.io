5f4513f77afb16d956a56a1c6a9b7bbd6051e896
Sunil Hooda <Sunilhooda000@gmail.com>
2022-10-16 22:40:30 +0530

    Navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-import logo from "./logo.svg";
 import "./App.css";
+import { AppNavbar } from "./Components/Navbar";
+import "./AppStyles.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <div className="App">
+      <AppNavbar />
+    </div>
+  );
 }
 
 export default App;
